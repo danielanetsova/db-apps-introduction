ef25cb57cefb2a8527c9d6b541ccc7d30fedd5e3
Daniela Netsova <103558247+danielanetsova@users.noreply.github.com>
2024-05-20 21:27:12 +0300

    Update Constants.java

--- src/DBAppsIntroduction/ex/Constants.java
+++ src/DBAppsIntroduction/ex/Constants.java
@@ -5,6 +5,6 @@ package DBAppsIntroduction.ex;
      static final String USER_KEY = "user";
      static final String USER_VALUE = "root";
      static final String PASSWORD_KEY = "password";
-     static final String PASSWORD_VALUE = "pass";
+     static final String PASSWORD_VALUE = "{yourPassword}";
      static final String JDBC_MYSQL_URL = "jdbc:mysql://localhost:3306/minions_db";
  }
